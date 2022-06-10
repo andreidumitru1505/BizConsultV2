422f8ebda9efff40a27a7ce79bf8fac87c351b33
Andrei <dumitru.andrei1505@gmail.com>
2022-06-10 18:48:58 +0300

    Industry Recommendation page styling

--- frontend/bizconsult/src/components/IndustryRecommendation.js
+++ frontend/bizconsult/src/components/IndustryRecommendation.js
@@ -3,6 +3,20 @@ import { useLocation } from 'react-router-dom';
 import Navbar from './Navbar';
 import * as constants from "../constants";
 
+const industries = [
+    {name: 'Artificial intelligence', value: 1, imgSrc: constants.artificialIntelligenceImg, description: constants.artificialIntelligenceDescription},
+    {name: 'Auto & transportation', value: 2, imgSrc: constants.autoTransportationImg, description: constants.autoTransportationDescription},
+    {name: 'Edtech', value: 3, imgSrc: constants.edtechImg, description: constants.edtechDescriptin},
+    {name: 'Internet Services', value: 4, imgSrc: constants.internetServicesImg, description: constants.internetServicesDescription},
+    {name: 'Fintech', value: 5, imgSrc: constants.fintechImg, description: constants.fintechDescription},
+    {name: 'Hardware', value: 6, imgSrc: constants.hardwareImg, description: constants.hardwareDescription},
+    {name: 'Health', value: 7, imgSrc: constants.healthImg, description: constants.healthDescription},
+    {name: 'Supply chain, logistics, & delivery', value: 8, imgSrc: constants.logisticsImg, description: constants.logisticsDescription},
+    {name: 'Consumer & retail', value: 9, imgSrc: constants.retailImg, description: constants.retailDescription},
+    {name: 'E-commerce & direct-to-consumer', value: 10, imgSrc: constants.ecommerceImg, description: constants.ecommerceDescription},
+    {name: 'Travel', value: 11, imgSrc: constants.travelImg, description: constants.travelDescription},
+    {name: 'Law Consultation', value: 12, imgSrc: constants.legalServicesImg, description: constants.legalServicesDescription}
+]
 
 async function post(requestData) {
     return fetch('http://localhost:8080/getRecommendation',{
@@ -18,6 +32,7 @@ async function post(requestData) {
 const IndustryRecommendation = () => {
 
     const {state} = useLocation();
+    const [industry, setIndustry] = useState({name: 'Artificial intelligence', value: 1, imgSrc: constants.artificialIntelligenceImg});
 
     const handleSubmit = async e => {
         e.preventDefault();
@@ -25,6 +40,7 @@ const IndustryRecommendation = () => {
         try {
 
             await post({
+                emailAddress: state.emailAddress
             });
         } catch (e) {
         }
@@ -41,31 +57,34 @@ const IndustryRecommendation = () => {
             <div class="py-16 bg-purple-200">  
     <div class="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
         <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-2">
-            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 h-3/5 overflow-auto">
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
+            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 h-1/4 overflow-auto">
+                <div aria-label="group of cards" tabindex="0" class="focus:outline-none py-8 w-full">
+                    {
+                        industries.map((item) => (
+                            <div class="lg:flex items-center justify-center w-full mt-7">
+                                <div aria-label="card 3" tabindex="0" class="focus:outline-none lg:mb-0 mb-7 bg-white p-6 shadow rounded w-full">
+                                    <div class="items-center justify-center flex border-b border-gray-200 text-white bg-orange-500 px-4 py-1 text-4xl rounded leading-loose font-semibold">
+                                        <button onClick={() => (setIndustry(item))}>
+                                            <span title="">
+                                                {item.name}
+                                            </span>
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
-                </div>
-                <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                    <a href="#" class="block font-medium text-purple-600">Know more</a>
-                </div>
-                <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg" class="w-2/3 ml-auto -mb-12" alt="illustration" loading="lazy" width="900" height="600"/>
             </div>
-            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 h-3/5">
+            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8 h-1/4">
+                <div class="mb-4 flex flex-wrap justify-center">
+                    <img src={industry.imgSrc} class="max-w-full h-56 rounded-lg" alt=""/>
+                </div>
                 <div class="mb-12 space-y-4">
-                    <h3 class="text-2xl font-semibold text-purple-900">UI Design</h3>
-                    <p class="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
+                    <h3 class="text-2xl font-semibold text-purple-900">{industry.name}</h3>
+                    <p class="mb-6">{industry.description}</p>
                     <a href="#" class="block font-medium text-purple-600">Know more</a>
                 </div>
-                <img src="https://tailus.io/sources/blocks/end-image/preview/images/ui-design.svg" class="w-2/3 ml-auto" alt="illustration" loading="lazy" width="900" height="600"/>
             </div>
         </div>
     </div>
--- frontend/bizconsult/src/constants.js
+++ frontend/bizconsult/src/constants.js
@@ -1,3 +1,42 @@
+import artificialIntelligenceImage from './images/artificialIntelligence.jpg'
+import autoTransportationImage from './images/autoTransportation.png'
+import edtechImage from './images/edtech.jpg'
+import internetServicesImage from './images/internetServices.jpg'
+import fintechImage from './images/fintech.webp'
+import hardwareImage from './images/hardware.jpg'
+import healthImage from './images/health.jpeg'
+import logisticsImage from './images/logistics.jpg'
+import retailImage from './images/retail.jpeg'
+import ecommerceImage from './images/ecommerce.jpg'
+import travelImage from './images/travel.jpg'
+import legalServicesImage from './images/legalServices.webp'
+
 export const ENTREPRENEUR_ROLE = "ENTREPRENEUR";
 export const EXPERT_ADMIN_ROLE = "EXPERT_ADMIN";
 export const SYS_ADMIN_ROLE = "SYS_ADMIN";
+
+export const artificialIntelligenceImg = artificialIntelligenceImage
+export const autoTransportationImg = autoTransportationImage
+export const edtechImg = edtechImage
+export const internetServicesImg = internetServicesImage
+export const fintechImg = fintechImage
+export const hardwareImg = hardwareImage
+export const healthImg = healthImage
+export const logisticsImg = logisticsImage
+export const retailImg = retailImage
+export const ecommerceImg = ecommerceImage
+export const travelImg = travelImage
+export const legalServicesImg = legalServicesImage
+
+export const artificialIntelligenceDescription = "Artificial intelligence is the simulation of human intelligence processes by machines, especially computer systems. Specific applications of AI include expert systems, natural language processing, speech recognition and machine vision."
+export const autoTransportationDescription = "Automotive and Transportation industry focuses on creating means of transportation for people and cargo and the key infrastructure reliable for the transfers."
+export const edtechDescriptin = "EdTech (a combination of `education` and `technology`) refers to hardware and software designed to enhance teacher-led learning in classrooms and improve students' education outcomes. EdTech is still in the early stages of its development, but it shows promise as a method of customizing a curriculum for a student’s ability level by introducing and reinforcing new content at a pace the student can handle."
+export const internetServicesDescription = "In order to connect to the Internet, you need to use an Internet service. Internet service provides a way for data to be transferred from Internet servers to your computer."
+export const fintechDescription = 'Fintech, or financial technology, is the term used to describe any technology that delivers financial services through software, such as online banking, mobile payment apps or even cryptocurrency.'
+export const hardwareDescription = 'The global IT hardware industry involves the assembly and manufacture of computers, along with their hardware and peripherals. The industry encompasses many devices including ATM machines, monitors, printers & copiers, keyboards, storage devices and webcams.'
+export const healthDescription = 'The healthcare industry (also called the medical industry or health economy) is an aggregation and integration of sectors within the economic system that provides goods and services to treat patients with curative, preventive, rehabilitative, and palliative care.'
+export const logisticsDescription = 'Logistics is the process of planning and executing the efficient transportation and storage of goods from the point of origin to the point of consumption. The goal of logistics is to meet customer requirements in a timely, cost-effective manner.'
+export const retailDescription = 'The retail industry consists of all companies that sell goods and services to consumers. There are many different retail sales and store types worldwide, including grocery, convenience, discounts, independents, department stores, DIY, electrical and speciality.'
+export const ecommerceDescription = 'E-commerce (electronic commerce) is the buying and selling of goods and services, or the transmitting of funds or data, over an electronic network, primarily the internet. These business transactions occur either as business-to-business (B2B), business-to-consumer (B2C), consumer-to-consumer or consumer-to-business.'
+export const travelDescription = 'The travel industry is concerned with providing services related to travel from one location to another. This includes services that are directly related to the travel itself, such as transportation, but also includes services related to catering for travellers’ needs and wants after they have arrived at their destination.'
+export const legalServicesDescription = 'Legal services are the services involving legal or law related matters like issue of legal opinion, filing, pleading and defending of law suits by a lawyer or attorney practicing law related services.'

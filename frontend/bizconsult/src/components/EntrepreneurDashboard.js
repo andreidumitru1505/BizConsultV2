57113d1b7d21a554a89ccc543960982db19cc075
Andrei <dumitru.andrei1505@gmail.com>
2022-06-11 19:13:26 +0300

    Entrepreneur Dashboard styling and backend info integration

--- frontend/bizconsult/src/components/EntrepreneurDashboard.js
+++ frontend/bizconsult/src/components/EntrepreneurDashboard.js
@@ -1,20 +1,43 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import {useEffect, useState} from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Navbar from './Navbar';
 
 const EntrepreneurDashboard = () => {
     const {state} = useLocation();
 
+    const [entrepreneurInfo, setEntrepreneurInfo] = useState();
+    const [isLoading, setIsLoading] = useState(1);
+    const emailAddress = state.emailAddress;
+    const dummy = 'dummyIndustry';
+    const navigate = useNavigate();
 
+    useEffect(() => {
+        console.log(state);
+        fetch('http://localhost:8080/getDashboardInfo',{
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({emailAddress, dummy})
+        })
+            .then(response => response.json())
+            .then(data => {setEntrepreneurInfo(data[0]);setIsLoading(0)})
+
+    }, []);
+
+
+    if(isLoading){
+        return <div>Loading...</div>
+    }
+    else{
 
     return (
         <div>
             <div>
                 <Navbar firstName={state.firstName} lastName={state.lastName} emailAddress={state.emailAddress} role={state.role}/>
             </div>
-
             <div class="flex bg-gray-100 rounded-xl m-3 shadow-xl">
                 <aside class="flex px-16 space-y-16 overflow-hidden m-3 pb-4 flex-col items-center justify-center rounded-tl-xl rounded-bl-xl bg-blue-800 shadow-lg">
                     <div class="flex items-center justify-center p-4 shadow-lg">
@@ -26,13 +49,21 @@ const EntrepreneurDashboard = () => {
                     <ul>
                     <li class="flex space-x-2 mt-4 px-6 py-4 text-white hover:bg-white hover:text-blue-800 font-bold hover:rounded-br-3xl transition duration-100 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" /></svg
-                        ><a href="">Dashboard</a>
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4" />
+                        </svg>
+                        <button  onClick={() => navigate("/entrepreneurDashboard",
+                                        {state:{firstName:state.firstName, lastName:state.lastName, emailAddress:state.emailAddress, role:state.role}})}>
+                                        Dashboard
+                        </button>
                     </li>
                     <li class="flex space-x-2 mt-4 px-6 py-4 text-white hover:bg-white hover:text-blue-800 font-bold hover:rounded-br-3xl transition duration-100 cursor-pointer">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8V4m0 0h4M4 4l5 5m11-1V4m0 0h-4m4 0l-5 5M4 16v4m0 0h4m-4 0l5-5m11 5l-5-5m5 5v-4m0 4h-4" /></svg
-                        ><a href="">Attendance</a>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" viewBox="0 0 24 24" fill="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z" />
+                        </svg>
+                        <button  onClick={() => navigate("/entrepreneurIdeas",
+                                        {state:{firstName:state.firstName, lastName:state.lastName, emailAddress:state.emailAddress, role:state.role}})}>
+                                        Ideas
+                        </button>
                     </li>
                     <li class="flex space-x-2 mt-4 px-6 py-4 text-white hover:bg-white hover:text-blue-800 font-bold hover:rounded-br-3xl transition duration-100 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -63,86 +94,95 @@ const EntrepreneurDashboard = () => {
                     </ul>
                 </aside>
                 <main class="flex-col bg-indigo-50 w-full ml-4 pr-6">
-                    <div class="flex justify-between p-4 bg-white mt-3 rounded-xl shadow-lg">
-                    <h1 class="text-xl font-bold text-gray-700">Welcome, {state.firstName}</h1>
-                    <div class="flex justify-between w-2/5">
-                        <div class="flex items-center border-2 p-2 rounded-xl">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                        </svg>
-                        <input type="text" placeholder="Search" class="ml-2 outline-none w-full" />
-                        </div>
-                        <div class="flex items-center space-x-6 pr-8">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 cursor-pointer text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
-                        </svg>
-                        <img src="https://i.imgur.com/iH7hkQb.png" alt="" class="cursor-pointer" />
-                        </div>
-                    </div>
-                    </div>
+                <div class="flex p-4 bg-white items-center mt-3 rounded-xl shadow-lg">
+                    <h1 class="text-4xl font-bold text-gray-700">Welcome, {state.firstName}</h1>
+                </div>
                     <div class="flex justify-between mt-4 space-x-4 s">
-                    <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
-                        <img src="https://i.imgur.com/VHc5SJE.png" alt="" />
-                        <div class="text-center">
-                        <h1 class="text-4xl font-bold text-gray-800">534</h1>
-                        <span class="text-gray-500">Coaches</span>
+                        <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 text-indigo-500" fill="none" viewBox="0 0 24 24" height="100px" width="100px" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
+                            </svg>               
+                            <div class="text-center">
+                                <h1 class="text-4xl font-bold text-gray-800">{entrepreneurInfo.companiesNo}</h1>
+                                <span class="text-gray-500">Companies Integrated</span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
-                        <img src="https://i.imgur.com/Qnmqkil.png" alt="" />
-                        <div class="text-center">
-                        <h1 class="text-4xl font-bold text-gray-800">9.7k</h1>
-                        <span class="text-gray-500">Kids</span>
+                        <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 text-indigo-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M11 3a1 1 0 10-2 0v1a1 1 0 102 0V3zM15.657 5.757a1 1 0 00-1.414-1.414l-.707.707a1 1 0 001.414 1.414l.707-.707zM18 10a1 1 0 01-1 1h-1a1 1 0 110-2h1a1 1 0 011 1zM5.05 6.464A1 1 0 106.464 5.05l-.707-.707a1 1 0 00-1.414 1.414l.707.707zM5 10a1 1 0 01-1 1H3a1 1 0 110-2h1a1 1 0 011 1zM8 16v-1h4v1a2 2 0 11-4 0zM12 14c.015-.34.208-.646.477-.859a4 4 0 10-4.954 0c.27.213.462.519.476.859h4.002z" />
+                            </svg>
+                            <div class="text-center">
+                                <h1 class="text-4xl font-bold text-gray-800">{entrepreneurInfo.ideasNo}</h1>
+                                <span class="text-gray-500">Ideas Generated</span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
-                        <img src="https://i.imgur.com/dJeEVcO.png" alt="" />
-                        <div class="text-center">
-                        <h1 class="text-4xl font-bold text-gray-800">50 M</h1>
-                        <span class="text-gray-500">Revenue</span>
-                        </div>
-                    </div>
-                    </div>
-                    <div class="flex space-x-4">
-                    <div class="justify-between rounded-xl mt-4 p-4 bg-white shadow-lg">
-                        <h1 class="text-xl font-bold text-gray-800 mt-4">Today’s Status</h1>
-                        <div class="flex justify-between space-x-4 text-center mt-6">
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>8.7K</h3>
-                            <spnan>Total Present</spnan>
-                        </div>
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>99</h3>
-                            <spnan>Registrations</spnan>
-                        </div>
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>30</h3>
-                            <spnan>Totals Session</spnan>
-                        </div>
+                        <div class="bg-white w-1/3 rounded-xl shadow-lg flex items-center justify-around">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-20 w-20 text-indigo-500" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clip-rule="evenodd" />
+                            </svg>
+                            <div class="text-center">
+                                <h1 class="text-4xl font-bold text-gray-800">{entrepreneurInfo.earnings}</h1>
+                                <span class="text-gray-500">Profit</span>
+                            </div>
                         </div>
                     </div>
                     <div class="justify-between rounded-xl mt-4 p-4 bg-white shadow-lg">
-                        <h1 class="text-xl font-bold text-gray-800 mt-4">Today’s Status</h1>
-                        <div class="flex justify-between space-x-4 text-center mt-6">
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>8.7K</h3>
-                            <spnan>Total Present</spnan>
-                        </div>
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>99</h3>
-                            <spnan>Registrations</spnan>
-                        </div>
-                        <div class="bg-indigo-50 rounded-xl p-10">
-                            <h3>30</h3>
-                            <spnan>Totals Session</spnan>
-                        </div>
-                        </div>
-                    </div>
+                        <section class="container mx-auto p-6 font-mono">
+                            <div class="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
+                                <div class="w-full overflow-x-auto">
+                                    <h2 class="mb-10 font-mono text-3xl font-bold">Your Companies overview</h2>
+                                    <table class="w-full">
+                                        <thead>
+                                            <tr class="text-md font-semibold tracking-wide text-center text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
+                                                <th class="px-4 py-3">Name</th>
+                                                <th class="px-4 py-3">Industry</th>
+                                                <th class="px-4 py-3">Size</th>
+                                                <th class="px-4 py-3">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody class="bg-white">
+                                            {
+                                                entrepreneurInfo.companies.map((item) => (
+                                                    <tr class="text-gray-700">
+                                                        <td class="px-4 py-3 border">
+                                                            <div class="flex items-center text-sm">
+                                                                <div>
+                                                                    <p class="font-semibold text-black">Company Name</p>
+                                                                </div>
+                                                            </div>
+                                                        </td>
+                                                        <td class="px-4 py-3 text-xs font-semibold border">{item.industry}</td>
+                                                        <td class="px-4 py-3 text-xs font-semibold border">{item.size}</td>
+                                                        {item.status === 'Active' &&
+                                                            <td class="px-4 py-3 text-xs border">
+                                                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-sm"> {item.status} </span>
+                                                            </td>
+                                                        }
+                                                        {(item.status === 'Under Review' || item.status === 'Awaiting Review') &&
+                                                            <td class="px-4 py-3 text-xs border">
+                                                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-gray-100 rounded-sm"> {item.status} </span>
+                                                            </td>
+                                                        }
+                                                        {!item.isActive &&
+                                                            <td class="px-4 py-3 text-xs border">
+                                                                <span class="px-2 py-1 font-semibold leading-tight text-green-700 bg-red-100 rounded-sm"> {item.status} </span>
+                                                            </td>
+                                                        }
+                                                    </tr>
+                                                ))
+                                            }
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </section>
                     </div>
                 </main>
-            </div>
+            </div>          
         </div>
-    )
+        )
+    }
 }
 
 

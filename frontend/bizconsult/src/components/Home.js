b0c2108ae8ee590a2b598d1ba873247f02d49641
Andrei <dumitru.andrei1505@gmail.com>
2022-06-18 15:25:03 +0300

    Frontend Sign Up Profile Type picker and Home Page

--- frontend/bizconsult/src/components/Home.js
+++ frontend/bizconsult/src/components/Home.js
@@ -2,7 +2,10 @@ import {React, useEffect, useState} from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useLocation } from "react-router-dom";
 import Navbar from './Navbar';
-import homeImage from '../images/home.png'
+import logo from '../images/BizConsult-logo.png'
+import background from '../images/Background-collage.png'
+import * as constants from '../constants';
+
 
 const Home = () => {
     const { isLoading } = useAuth0();
@@ -13,18 +16,34 @@ const Home = () => {
     }
     return (
     <>
-        
         <div>
             <Navbar firstName={state.firstName} lastName={state.lastName} emailAddress={state.emailAddress} role={state.role}/>
         </div>
-            <div style={{maxWidth: "54px"}}>  </div>
-        <div style ={{display: "flex", justifyContent:"center",alignItems: "center",marginTop:"50px"}}> 
-            <div class="container">
-            <img className="img" src={homeImage}  style ={{display: "block", display: "flex", justifyContent:"center", alignItems: "center", margin: " 0 auto", borderRadius: "20px"}} />
+        <div class="h-screen pb-14 bg-right bg-cover bg-opacity-0" style={{ backgroundImage: `url(${background})`}}>
+            <div class="w-full container mx-auto ">
+                <div class="w-full flex items-center justify-between">
+                    <div class="flex w-1/2 content-center">		
+                        <div class="text-indigo-500 md:order-1">
+                            <img className="img h-80 w-80 mb-0" src={logo}/>
+                        </div>
+                    </div>
+                </div>
             </div>
-           
-        </div>   
-        
+            <div class="container px-6 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
+                    <h1 class="my-4 text-3xl md:text-5xl text-purple-800 font-bold leading-tight text-center md:text-left slide-in-bottom-h1">Business counseling tailored for your needs</h1>
+                    <p class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left slide-in-bottom-subtitle">At BizConsult, we make it our purpose to help your company succeed through our continously improving business idea generator and rich partner community.</p>
+                    { state.role !== constants.EXPERT_ADMIN_ROLE &&
+                        state.role !== constants.ENTREPRENEUR_ROLE &&
+                        state. role !== constants.SYS_ADMIN_ROLE && 
+                    <p class="text-indigo-500 font-bold pt-10 pb-8 lg:pb-6 text-center md:text-left fade-in">Want to join in on the fun? Create your account today and start building your future unicorn!</p>
+                    }
+                </div>
+                <div class="w-full pt-16 pb-6 text-sm text-center md:text-left fade-in">
+                    <a class="text-gray-500 no-underline hover:no-underline" href="#">&copy; BizConsult 2022</a>
+                </div>
+            </div>
+        </div> 
     </>
     )
 };
--- frontend/bizconsult/src/components/Navbar.js
+++ frontend/bizconsult/src/components/Navbar.js
@@ -3,7 +3,7 @@ import LogoutButton from "./buttons/LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import './Navbar.css';
 import { useNavigate } from "react-router-dom";
-import logo from '../images/logo.png'
+import logo from '../images/BizConsult-logo.png'
 import * as constants from '../constants';
 
 
@@ -17,11 +17,7 @@ const Navbar = (props) => {
             <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                 <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                     <div class="text-indigo-500 md:order-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                        </svg>
+                        <img className="img h-36 w-36 mb-0" src={logo}/>
                     </div>
                     <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
                         <ul class="flex font-semibold justify-between">
@@ -58,11 +54,7 @@ const Navbar = (props) => {
                 <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                     <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                         <div class="text-indigo-500 md:order-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                            </svg>
+                            <img className="img h-36 w-36 mb-0" src={logo}/>
                         </div>
                         <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
                             <ul class="flex font-semibold justify-between">
@@ -92,11 +84,7 @@ const Navbar = (props) => {
                 <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                     <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                         <div class="text-indigo-500 md:order-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                            </svg>
+                            <img className="img h-36 w-36 mb-0" src={logo}/>
                         </div>
                         <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
                             <ul class="flex font-semibold justify-between">
@@ -122,11 +110,7 @@ const Navbar = (props) => {
                 <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                     <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                         <div class="text-indigo-500 md:order-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                            </svg>
+                            <img className="img h-36 w-36 mb-0" src={logo}/>
                         </div>
                         <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
                             <ul class="flex font-semibold justify-between">
@@ -150,11 +134,7 @@ const Navbar = (props) => {
                 <nav class="bg-gray-200 shadow shadow-gray-300 w-100 px-8 md:px-auto">
                     <div class="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
                         <div class="text-indigo-500 md:order-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
-                            </svg>
+                            <img className="img h-36 w-36 mb-0" src={logo}/>
                         </div>
                         <div class="text-gray-500 order-3 w-full md:w-auto md:order-2">
                             <ul class="flex font-semibold justify-between">
